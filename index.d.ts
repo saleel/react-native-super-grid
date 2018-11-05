fcdf5b27ebc8c9ae82cf60104669c458f3758987
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2018-11-05 11:19:53 +0200

    Update declaration by adding Index for each element

--- index.d.ts
+++ index.d.ts
@@ -8,7 +8,7 @@ export interface SuperGridProps<ItemType = any> {
   /**
    * Function to render each object. Should return a react native component.
    */
-  renderItem: (item: ItemType) => JSX.Element;
+  renderItem: (item: ItemType, index: number) => JSX.Element;
 
   /**
    * Items to be rendered. renderItem will be called with each item in this array.
