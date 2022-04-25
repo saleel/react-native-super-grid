3c717e162f9063ef2787b6ccc20139e9515bf5f3
levic92 <1757742+levic92@users.noreply.github.com>
2022-04-24 22:51:51 -0700

    Also adjust totalDimension for Grid `style` prop padding
    If padding is set in the `style` prop of the grid then the `totalDimension` should reflect that

--- utils.js
+++ utils.js
@@ -42,49 +42,82 @@ function calculateDimensions({
   };
 }
 
+function getStyleDimensions(
+  style,
+  horizontal = false,
+) {
+  let space1 = 0;
+  let space2 = 0;
+  let maxStyleDimension = undefined;
+  if (style) {
+    const flatStyle = Array.isArray(style) ? StyleSheet.flatten(style) : style;
+    let sMaxDimensionXY = 'maxWidth';
+    let sPaddingXY = 'paddingHorizontal';
+    let sPadding1 = 'paddingLeft';
+    let sPadding2 = 'paddingRight';
+    if (horizontal) {
+      sMaxDimensionXY = 'maxHeight';
+      sPaddingXY = 'paddingVertical';
+      sPadding1 = 'paddingTop';
+      sPadding2 = 'paddingBottom';
+    }
+
+    if (flatStyle[sMaxDimensionXY] && typeof flatStyle[sMaxDimensionXY] === 'number') {
+      maxStyleDimension = flatStyle[sMaxDimensionXY];
+    }
+
+    const padding = flatStyle[sPaddingXY] || flatStyle.padding;
+    const padding1 = flatStyle[sPadding1] || padding || 0;
+    const padding2 = flatStyle[sPadding2] || padding || 0;
+    space1 = (typeof padding1 === 'number' ? padding1 : 0);
+    space2 = (typeof padding2 === 'number' ? padding2 : 0);
+  }
+  return { space1, space2, maxStyleDimension };
+}
+
 function adjustDimension({
   newTotalDimension,
   maxDimension,
   contentContainerStyle,
+  style,
   horizontal = false,
 }) {
-  let sMaxDimensionXY = 'maxWidth';
-  let sPaddingXY = 'paddingHorizontal';
-  let sPadding1 = 'paddingLeft';
-  let sPadding2 = 'paddingRight';
-  if (horizontal) {
-    sMaxDimensionXY = 'maxHeight';
-    sPaddingXY = 'paddingVertical';
-    sPadding1 = 'paddingTop';
-    sPadding2 = 'paddingBottom';
-  }
+  const componentDimension = newTotalDimension; // keep track of initial max of component/screen
+  let actualMaxDimension = newTotalDimension; // keep track of smallest max dimension
 
   // adjust for maxDimension prop
   if (maxDimension && newTotalDimension > maxDimension) {
+    actualMaxDimension = maxDimension;
     newTotalDimension = maxDimension;
   }
 
   if (contentContainerStyle) {
-    contentContainerStyle = Array.isArray(contentContainerStyle) ? StyleSheet.flatten(contentContainerStyle) : contentContainerStyle;
-
+    const { space1, space2, maxStyleDimension } = getStyleDimensions(contentContainerStyle, horizontal);
     // adjust for maxWidth or maxHeight in contentContainerStyle
-    if (contentContainerStyle[sMaxDimensionXY] && typeof contentContainerStyle[sMaxDimensionXY] === 'number' && newTotalDimension > contentContainerStyle[sMaxDimensionXY]) {
-      newTotalDimension = contentContainerStyle[sMaxDimensionXY];
+    if (maxStyleDimension && newTotalDimension > maxStyleDimension) {
+      actualMaxDimension = maxStyleDimension;
+      newTotalDimension = maxStyleDimension;
     }
-
     // subtract horizontal or vertical padding from newTotalDimension
-    const padding = contentContainerStyle[sPaddingXY] || contentContainerStyle.padding;
-    const padding1 = contentContainerStyle[sPadding1] || padding || 0;
-    const padding2 = contentContainerStyle[sPadding2] || padding || 0;
-    const space1 = (typeof padding1 === 'number' ? padding1 : 0);
-    const space2 = (typeof padding2 === 'number' ? padding2 : 0);
     if (space1 || space2) {
       newTotalDimension = newTotalDimension - space1 - space2;
     }
   }
 
+  if (style) {
+    const edgeSpaceDiff = (componentDimension - actualMaxDimension) / 2; // if content is floating in middle of screen get margin on either side
+    const { space1, space2 } = getStyleDimensions(style, horizontal);
+    // only subtract if space is greater than the margin on either side
+    if (space1 > edgeSpaceDiff) {
+      newTotalDimension = newTotalDimension - (space1 - edgeSpaceDiff); // subtract the padding minus any remaining margin
+    }
+    if (space2 > edgeSpaceDiff) {
+      newTotalDimension = newTotalDimension - (space2 - edgeSpaceDiff); // subtract the padding minus any remaining margin
+    }
+  }
+
   return newTotalDimension;
-};
+}
 
 function generateStyles({
   itemDimension,
